d36245f30ea9db701580f3581f40245476884a99
trmtanjil <trmtanjil02@gmail.com>
2025-11-10 15:27:41 +0600

    Explore Primitive Types

---
+++ level-2-moddule-5-ts/src/primitive.ts
@@ -0,0 +1,24 @@
+// js - ts
+
+// string , number , boolean , undefined, null
+
+let userName :string = 'tanjil';
+
+// userName = 123;
+userName.charAt
+
+ let userId  = 123;
+
+ userId = '123'
+ userId.toFixed()
+
+
+ let boolean :boolean= true;
+ boolean = '123'
+
+
+ let z = undefined;
+ z = 1233;
+
+ let y :undefined = undefined;
+ y = ' klj'
