8d01e4a0970401c849926c58e6b191551e485641
trmtanjil <trmtanjil02@gmail.com>
2025-11-10 16:04:15 +0600

    Explore Non Primitive Types

---
+++ level-2-moddule-5-ts/dist/ExploreNonPrimitiveTypes.ts
@@ -0,0 +1,25 @@
+// array , object
+
+//ts 
+
+let bazarList :string[] = ['eggs', 'banana', 'apple']
+
+// bazarList.push(123)
+bazarList.push('tomato')
+
+let mixFood :(string | number)[] = ['eggs',2, 'banana',2, 'apple',3]
+mixFood.push(2,'appl')
+// mixFood.push(true)
+
+let coordinatess : [number, number]= [20, 30,]
+let coordinates : [number, number,number]= [20, 30,40]
+// let coordinatesss : [number, number,]= [20, 30,40]
+
+let couple : [string, string]= ['tanjil', 'wife']
+
+let tanjilNameAndRoll : [string, number]=['tanjil',12]
+
+tanjilNameAndRoll[0]= 'sdlk'
+// tanjilNameAndRoll[0]= 123
+
+let destination : [number, number,string]= [20, 30,'40']
